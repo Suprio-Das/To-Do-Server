62b4bac42db01e70a869e9ec76ebcc2100b8c67b
Suprio-Das <priyamd507@gmail.com>
2025-03-02 23:21:19 +0600

    Express Server created

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Backend')
+const express = require('express');
+const cors = require('cors');
+const PORT = 5000;
+
+// Creating Express App
+const app = express();
+
+// Using MiddleWares
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hola! Express server is running.');
+});
+
+app.listen(PORT);
